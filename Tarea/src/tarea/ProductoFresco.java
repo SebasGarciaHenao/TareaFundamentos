afe66731fc8fe67001494a1796cb50b48257effe
SebasGH <110042473+SebasGarciaHenao@users.noreply.github.com>
2024-05-20 22:11:42 -0500

    Add files via upload
    Final

---
+++ Tarea/src/tarea/ProductoFresco.java
@@ -0,0 +1,44 @@
+package tarea;
+
+import java.util.Scanner;
+
+public class ProductoFresco extends Producto {
+
+   private String organismoDesupervisionAlimentario;
+
+    public ProductoFresco(){
+
+    }
+
+    public ProductoFresco(String fechaCaducidad, int numeroLote, float temperaturaRecomendada){
+        super(fechaCaducidad, numeroLote);
+        this.organismoDesupervisionAlimentario = organismoDesupervisionAlimentario;
+
+    }
+
+    public  String GetorganismoDesupervisionAlimentario(){
+        return organismoDesupervisionAlimentario;
+    }
+
+    public void SetorganismoDesupervisionAlimentario(){
+        this.organismoDesupervisionAlimentario = organismoDesupervisionAlimentario;
+    }
+
+    public void RegistrarProductoFresco(){
+        Scanner RegistrarProductoFresco = new Scanner(System.in);
+
+        System.out.println("Registrar producto fresco. ");
+        System.out.print("Ingrese organismo que supervisa el producto: ");
+        organismoDesupervisionAlimentario = RegistrarProductoFresco.next();
+        System.out.println("El organismo encargado de supervisar es : " +GetorganismoDesupervisionAlimentario());
+
+        System.out.println("El número de lote del producto es: " + GetnumeroLote());
+
+        System.out.println("La fecha de caduidad del producto es: " + GetfechaCaducidad());
+
+        System.out.println();
+
+
+    }
+}
+
