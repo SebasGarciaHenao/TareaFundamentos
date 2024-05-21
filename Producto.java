2973456ecb0b2c966e1ea5fe2edd56ecdbda518d
SebasGH <110042473+SebasGarciaHenao@users.noreply.github.com>
2024-05-20 20:25:08 -0500

    Add files via upload

---
+++ Producto.java
@@ -0,0 +1,48 @@
+package tarea;
+
+public class Producto {
+
+    public String fechaCaducidad;
+    public int numeroLote;
+
+
+    public Producto(){
+
+    }
+
+
+    public Producto( String fechaCaducidad, int numeroLote){
+        this.fechaCaducidad = fechaCaducidad;
+        this.numeroLote = numeroLote;
+
+    }
+
+
+
+    public void SetfechaCaducidad(String fechaCaducidad){
+        this.fechaCaducidad = fechaCaducidad;
+
+    }
+
+    public String GetfechaCaducidad(){
+        return "03/12/2025";
+    }
+
+    public void SetnumeroLote(int numeroLote){
+        this.numeroLote = numeroLote;
+    }
+
+    public int GetnumeroLote(){
+        return 45675556;
+    }
+
+
+    public static void  main (String[] args){
+
+        ProductoCongelador Con = new ProductoCongelador();
+
+        Con.RegistrarProductoCongelador();
+
+    }
+
+}
